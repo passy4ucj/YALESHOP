2df8c37af868b7a2c33df324462662ceea7e48c2
passy4ucj <ojinnnakapascal@gmail.com>
2020-11-08 18:57:04 +0100

    Order Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,7 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer} from './reducers/productReducer'
 import { cartReducer} from './reducers/cartReducer'
 import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from './reducers/userReducer'
-import { orderCreateReducer } from './reducers/orderReducer'
+import { orderCreateReducer, orderDetailsReducer } from './reducers/orderReducer'
 
 
 const  reducer = combineReducers({
@@ -15,7 +15,8 @@ const  reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
-    orderCreate: orderCreateReducer
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer,
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') 
