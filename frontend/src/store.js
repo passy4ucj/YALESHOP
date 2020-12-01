bf641c18fdb94ee4dfa346f0e56114d7079224cb
passy4ucj <ojinnnakapascal@gmail.com>
2020-12-01 19:07:59 +0100

    Admin Create Product

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware  } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productDetailsReducer, productDeleteReducer} from './reducers/productReducer'
+import { productListReducer, productDetailsReducer, productDeleteReducer, productCreateReducer} from './reducers/productReducer'
 import { cartReducer} from './reducers/cartReducer'
 import { userDeleteReducer, userDetailsReducer, userListReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer, userUpdateReducer } from './reducers/userReducer'
 import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer } from './reducers/orderReducer'
@@ -11,6 +11,7 @@ const  reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     productDelete: productDeleteReducer,
+    productCreate: productCreateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
